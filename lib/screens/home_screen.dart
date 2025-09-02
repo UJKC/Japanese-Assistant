139abc27ee3cb04643ad40bd5e5e870a9455b72f
Ujwal KC <ujwalkcsps@gmail.com>
2025-09-02 10:47:09 +0000

    Home screen good looking

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,3 @@
-// lib/screens/home_screen.dart
 import 'package:flutter/material.dart';
 import 'package:japanese_assistant/screens/custom_quiz_screen.dart';
 import 'flashcard_main.dart';
@@ -10,31 +9,109 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Japanese Flashcards")),
-      body: Center(
-        child: Column(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.deepPurple, Colors.purpleAccent],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Japanese Flashcards",
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(height: 40),
+
+              // Flashcards Button
+              _buildMenuButton(
+                context,
+                title: "Learn Flashcards",
+                icon: Icons.book,
+                color: Colors.orange,
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => const FlashcardMain()),
+                ),
+              ),
+
+              const SizedBox(height: 20),
+
+              // Quiz Button
+              _buildMenuButton(
+                context,
+                title: "Take Quiz",
+                icon: Icons.quiz,
+                color: Colors.blue,
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => const QuizScreen()),
+                ),
+              ),
+
+              const SizedBox(height: 20),
+
+              // Custom Quiz Button
+              _buildMenuButton(
+                context,
+                title: "Custom Quiz",
+                icon: Icons.edit,
+                color: Colors.green,
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => const CustomQuizScreen()),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildMenuButton(
+    BuildContext context, {
+    required String title,
+    required IconData icon,
+    required Color color,
+    required VoidCallback onTap,
+  }) {
+    return InkWell(
+      onTap: onTap,
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
+        width: 240,
+        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(16),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.2),
+              blurRadius: 6,
+              offset: const Offset(0, 4),
+            ),
+          ],
+        ),
+        child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ElevatedButton(
-              child: const Text("Learn Flashcards"),
-              onPressed: () => Navigator.push(
-                context,
-                MaterialPageRoute(builder: (_) => const FlashcardMain()),
-              ),
-            ),
-            ElevatedButton(
-              child: const Text("Take Quiz"),
-              onPressed: () => Navigator.push(
-                context,
-                MaterialPageRoute(builder: (_) => const QuizScreen()),
-              ),
-            ),
-            const SizedBox(height: 20), // spacing
-            ElevatedButton(
-              child: const Text("Custom Quiz"), // NEW
-              onPressed: () => Navigator.push(
-                context,
-                MaterialPageRoute(builder: (_) => const CustomQuizScreen()),
+            Icon(icon, color: color, size: 26),
+            const SizedBox(width: 12),
+            Text(
+              title,
+              style: TextStyle(
+                color: color,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
               ),
             ),
           ],
