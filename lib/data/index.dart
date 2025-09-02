636f4435f499d8263a05da0caa2b7679d39830a9
Ujwal KC <ujwalkcsps@gmail.com>
2025-09-02 09:43:07 +0000

    Ok let me see about pronounciation part of this.

--- lib/data/index.dart
+++ lib/data/index.dart
@@ -3,11 +3,13 @@
 // Import all lesson files here
 import 'lesson1.dart';
 import 'lesson20.dart';
+import 'lesson0.dart';
 import '../models/lesson.dart';
 
 // Export them if you also want direct access
 export 'lesson1.dart';
 export 'lesson20.dart';
+export 'lesson0.dart';
 
 // Now provide a single list that contains all lessons
-List<Lesson> lessons = [lesson1, lesson20];
+List<Lesson> lessons = [lesson1, lesson20, lesson0];
